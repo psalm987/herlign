5e100637eb069fa2a38d96855e1bbc4c94c1b5dc
Samuel Seibidor <samrythmn@gmail.com>
2026-01-04 17:45:39 +0100

    Refactor TestimonialsSection layout for improved structure and readability

--- components/sections/about/testimonial-section.tsx
+++ components/sections/about/testimonial-section.tsx
@@ -5,12 +5,14 @@ const TestimonialsSection = () => {
   return (
     <section className="py-8 sm:py-20 md:py-28 bg-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 lg:grid lg:grid-cols-2 gap-12">
-        <h2 className="font-heading text-5xl  md:text-7xl font-semibold text-gray-900 mb-4 text-center md:text-left">
-          The Proof is in the{" "}
-          <span className="font-handwriting text-perple-500 text-6xl md:text-8xl">
-            Passion
-          </span>
-        </h2>
+        <div className="h-full">
+          <h2 className="font-heading text-5xl  md:text-7xl font-semibold text-gray-900 mb-4 text-center md:text-left sticky top-28">
+            The Proof is in the{" "}
+            <span className="font-handwriting text-perple-500 text-6xl md:text-8xl">
+              Passion
+            </span>
+          </h2>
+        </div>
         <div className="grid gap-6 grid-cols-1 md:grid-cols-2 ">
           {/* Testimonial Cards */}
           {TESTIMONIALS.map((testimonial, index) => (

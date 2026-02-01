46f034c29f7fd77985a518f394756caba498f14a
Samuel Seibidor <samrythmn@gmail.com>
2026-02-01 04:03:08 +0100

    Refactor code structure for improved readability and maintainability

--- components/sections/home/hero-section.tsx
+++ components/sections/home/hero-section.tsx
@@ -6,7 +6,7 @@ import AnimateCreative from "./animate-creative";
 
 export default function HeroSection() {
   return (
-    <section className="relative pt-25 md:pt-20 pb-0 bg-linear-to-b bg-white overflow-hidden">
+    <section className="relative pt-25 md:pt-20 pb-0 bg-linear-to-b  from-white via-white via-70% to-peenk-100 overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative mb-30 md:mb-0">
         <div className="text-center max-w-4xl mx-auto">
           <h1 className="font-heading text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-gray-900 mb-6 text-pretty">
@@ -40,7 +40,7 @@ export default function HeroSection() {
         </div>
       </div>
       <Image
-        src="/images/webp/hero-image-super-enhanced.webp"
+        src="/images/webp/hero-image-enhanced.webp"
         alt=""
         width={1500}
         height={600}
